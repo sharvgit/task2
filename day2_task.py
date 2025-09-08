729d33f84f63dd0315e3b3dd437821fe2deaf359
sharvgit <jsarvanisarvani@gmail.com>
2025-09-08 12:09:11 +0530

    Add files via upload

---
+++ day2_task.py
@@ -0,0 +1,103 @@
+# write a python progarm that prints your name
+
+# print("sharvani")
+
+#create a python script with both single-multiline comments explainig the purpose of the script
+
+"""create a python script with both single-line and multi line comments explaing 
+the purpose of the script"""
+
+''' python is high level programming language, interactive and intrepeted programming
+language.python was deveoloped by " Guido van Russan ". It is a simple and readable.
+It is portable we can use in any device.
+
+'''
+
+""" Declare two variables ,one storing an integer another storing  an string element
+print their values  """
+
+# define a list containing three different data types 
+# list_1=[30 ,40.6 ,"laddu",["sarvani", 30 , 79.9]]
+# print(list_1)
+
+""" concatenate teo string and print the reslut """
+
+# num_1="20"
+# num_2="30"
+# reslut=num_1+num_2
+# print(reslut)
+
+"""repeat  a string three times display output """
+
+# print("!"*70)
+
+""" create a variable with a python keyword .what happens ?"""
+
+# yield="ASHA"
+# print(yield)
+
+"""declare two varaiables one is stroing in integer and storing in string print their values """
+# student_name="CHAKRADHAR RAO"
+# roll_no=28
+# print(student_name)
+# print(roll_no)
+""" convert a float  to an integer and print the reslut"""
+# width=45.0
+# int_conversion=int(width)
+# print(type(int_conversion))
+
+""" convert an integer to a string and display the reslut """
+# height=40 
+# string_1=str(height)
+# print(string_1)
+# print(type(string_1))
+
+""" take user's age as input and print a message using that input """
+# member_1=int(input("enter the age of the person:"))
+# print(f"member age is {member_1}")
+
+""" write a program  that prints a pattern using multiple print statements """
+
+
+# print("*******")
+# print("&&&&&&&&&&&")
+
+""" create a python script for a simple task add comments to explain each step"""
+# num_1=20# first value  20 
+# num_2=40  # second value 30
+# reslut=num_1+num_2 # later i added first value and second value
+# print(reslut)# inthis i got my reslut
+
+
+""" implement a program that uses a dictionary to store information about a book (title,author , publication year ) """
+
+# book_1={"TITLE":"AMMA DAIRY LO KOONI PAGGELU ",
+#         "AUTHOR":"RAVI MANTRI ",
+#         "PUBLICATION YEAR":"2023"}
+# print(book_1)
+
+""" write a program that takes a string as input (35) and return float value"""
+
+# width=input("enter the number:")
+# print(float(width))
+
+
+""" write a program that takes two names as input and print together """
+
+# first_person=input("enter the first name :")
+# second_person=input("enter the second name:")
+# print(first_person + second_person)
+
+""" CREATE PROGRAM THAT  TAKES USER INPUT FOR THEIR AGE , CONVERT IT TO AN INTEGER , ADD 5 AND THEN PRINT THE RESLUT """
+# person_age=int(input ("enter the age of the person "))
+# add= person_age+5
+# print(add)
+
+""" build a caluculator program that takes two numbers as input and performs an arthematic operation """
+# num_1=int(input("enter the first number :"))
+# num_2= int(input ("enter the second nnumber:"))
+# print(num_1+num_2)
+# print(num_1-num_2)
+# print(num_1*num_2)
+# print(num_1/num_2)
+# print(num_1%num_2)
